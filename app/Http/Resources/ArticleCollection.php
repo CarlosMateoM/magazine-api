c5401c6fd1b3bd1094fea427d5df1c0c5e1309fb
CarlosMateoM <carlosmateo484@gmail.com>
2024-02-14 15:01:13 -0500

    Revert "Revert "add: show category method and article  pagination""
    This reverts commit f952c8f4388249b84471ffb30d62c2c500076c90.

---
+++ app/Http/Resources/ArticleCollection.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ArticleCollection extends ResourceCollection
+{
+
+    public $collects = ArticleResource::class;
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'current_page' => $this->currentPage(),
+            'data' => $this->collection,
+            'first_page_url' => $this->url(1),
+            'from' => $this->firstItem(),
+            'last_page' => $this->lastPage(),
+            'last_page_url' => $this->url($this->lastPage()),
+            'next_page_url' => $this->nextPageUrl(),
+            'path' => $this->path(),
+            'per_page' => $this->perPage(),
+            'prev_page_url' => $this->previousPageUrl(),
+            'to' => $this->lastItem(),
+            'total' => $this->total(),
+        ];
+    }
+}
