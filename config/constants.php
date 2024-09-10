770754a2815a9849ca232f1e56037670f1af2510
CarlosMateoM <carlosmateo484@gmail.com>
2024-09-10 11:56:29 -0500

    add: constant file

---
+++ config/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Per Page
+    |--------------------------------------------------------------------------
+    |
+    | This value is the default number of models that will be shown per page
+    |
+    */
+    
+    'default_per_page' => 10
+];
