abd6ff273eff99e02502e19cc9899aae0225b54d
Javier E Montes <jaxloz2002@gmail.com>
2024-12-26 18:26:35 -0500

    ajuste en resorce DepartmentResource

--- app/Http/Resources/DepartmentResource.php
+++ app/Http/Resources/DepartmentResource.php
@@ -16,7 +16,8 @@ class DepartmentResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'name' => $this->name,  
+            'name' => $this->name,
+            'municipalities' => MunicipalityResource::collection($this->whenLoaded('municipalities')),
         ];
     }
 }
